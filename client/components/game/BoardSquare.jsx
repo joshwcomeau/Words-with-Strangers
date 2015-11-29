9e0f55748737336e312bad1290daf188d392c3ea
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-29 10:39:32 -0500

    Created a module to store core game logic stuff. Updated the dropTile method to randomly generate a new tile to replace the one just played, and add it to the player's rack.

--- client/components/game/BoardSquare.jsx
+++ client/components/game/BoardSquare.jsx
@@ -4,11 +4,18 @@ BoardSquare = React.createClass({
     // from the rack onto the clicked square.
     let selectedTile = Tiles.findOne({ location: 'rack' }, {sort: { position: 1 }});
 
+    // Move the tile to this place on the board
     Tiles.update(selectedTile._id, {
       $set: {
         location: 'board',
         position: [this.props.x, this.props.y]
       }
+    });
+
+    // Generate a new tile for the user
+    Modules.gameLogic.addTilesToPlayerRack({
+      playerId: Meteor.userId(),
+      gameId: this.props.gameId
     });
   },
   render() {
