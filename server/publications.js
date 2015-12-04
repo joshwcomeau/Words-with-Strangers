4f8762b15eaa2aaa33cb30f01f7494e314859c49
Joshua Comeau <joshwcomeau@gmail.com>
2015-12-04 09:07:20 -0500

    Added a User dependency to the 'game' publication, and made it reactive (so that when a player joins a game, all existing players get that user's info right away). Extracted it into a function since both current publications use it.

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,8 @@
-Meteor.publish('game', function(gameId) {
+Meteor.smartPublish('game', function(gameId) {
+  // Fetch all players of this game.
+  // Publish only the fields we need.
+  addPlayersDependencyToGame.call(this);
+
   return [
     Games.find({ _id: gameId }),
     Turns.find({ gameId: gameId}),
@@ -17,6 +21,17 @@ Meteor.publish('game', function(gameId) {
 Meteor.smartPublish('games', function(limit = 10) {
   // Fetch all players of a given game.
   // Publish only the fields we need.
+  addPlayersDependencyToGame.call(this);
+
+  return [
+    Games.find({ status: 'waiting' })
+  ];
+});
+
+function addPlayersDependencyToGame(userField, collection) {
+  // addDependency is a method provided by SmartPublish. Whenever an item
+  // of collection is published, it runs this dependency function to find
+  // any associated users, and returns the correct fields (profile and username).
   this.addDependency('games', 'playerIds', function(event) {
     return Meteor.users.find({
       _id: { $in: event.playerIds }
@@ -24,8 +39,4 @@ Meteor.smartPublish('games', function(limit = 10) {
       fields: { profile: 1, username: 1 }
     });
   });
-
-  return [
-    Games.find({ status: 'waiting' })
-  ];
-});
+}
