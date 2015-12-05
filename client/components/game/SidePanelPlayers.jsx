cd36f2169fcc4c275c132bb68adffa2eaa3d56c5
Josh Comeau <josh@breather.com>
2015-12-05 10:04:28 -0500

    Styled the players sidebar

--- client/components/game/SidePanelPlayers.jsx
+++ client/components/game/SidePanelPlayers.jsx
@@ -1,9 +1,13 @@
 SidePanelPlayers = React.createClass({
   renderPlayers() {
     return this.props.players.map( (player) => {
-      return (<div className="side-panel-player" key={player._id}>
-        {player.username} - {player.points(this.props.gameId)}
-      </div>);
+      return (
+        <div className="side-panel-player" key={player._id}>
+          <div className="avatar" style={{backgroundImage: `url('${player.profile.photo}')`}}></div>
+          <div className="username">{player.username}</div>
+          <div className="points">{player.points(this.props.gameId)}</div>
+        </div>
+      );
     });
   },
   render() {
